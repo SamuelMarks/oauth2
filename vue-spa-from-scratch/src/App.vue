cf7ac73ad49d97251737674b1a29fb1ab5651857
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-02-12 23:52:53 +0000

    fixed Vue from scratch

--- vue-spa-from-scratch/src/App.vue
+++ vue-spa-from-scratch/src/App.vue
@@ -14,72 +14,68 @@ import { store } from "./store"
 import AppID from "ibmcloud-appid-js"
 import { IBM_APP_ID_CLIENTID, IBM_APP_ID_DISCOVERY_ENDPOINT } from "./env.js"
 
-function save_state_and_verifier(state, code_verifier) {
+// here we build the authentication string that will send us off to IBM App ID
+const clientId = IBM_APP_ID_CLIENTID
+const discoveryEndpoint = IBM_APP_ID_DISCOVERY_ENDPOINT
+const jsrsasign = require('jsrsasign');
+
+// here we specify the return uri HOWEVER we could get this from the 
+// browsers window.location.href variable, which is what the 
+// ibmcloud-appid-js package does.
+const redirectUri='http://localhost:8080/auth'
+let authentication_url = null;
+
+function save_state_and_verifier(state, codeVerifier) {
   // save the state and verifier ready for reload after sign in
   if ( window.location.search.includes("state") ) return;
   const storage = window.sessionStorage;
   storage.clear();
   storage.setItem("state", state);
-  storage.setItem("code_verifier", code_verifier);
+  storage.setItem("codeVerifier", codeVerifier);
 }
 
 // required to bring in async/await babel functionality
 import 'regenerator-runtime/runtime'
-async function generateCodeChallenge(codeVerifier) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(codeVerifier);
-  const digest = await window.crypto.subtle.digest("SHA-256", data);
-  const base64Digest = base64encode(digest);
-  // you can extract this replacing code to a function
-  return base64Digest
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=/g, "");
+async function generatePKCECodeChallenge(codeVerifier) {
+  const codeChallenge = jsrsasign.stob64(jsrsasign.KJUR.crypto.Util.sha256(codeVerifier))
+  const nonce = getRandomString(20)
+  return {
+    codeVerifier: codeVerifier,
+    codeChallenge: codeChallenge,
+    nonce: nonce
+  }
 }
 
 // Proto-code. Don't use for production
-function buildLoginUrl(challenge) {
-  const link = document.querySelector("a");
-  const linkValue = new URL(baseUrl);
+function buildLoginUrl(state, challenge) {
+  const endpoint = "https://eu-gb.appid.cloud.ibm.com/oauth/v4/3143b98e-2035-4fac-b765-879402a1b585/authorization"
+  const nonce = challenge.nonce
 
   const queryParams = {
     client_id: clientId, 
     response_type: "code",
     state: state,
-    code_challenge: challenge,
+    code_challenge: challenge.codeChallenge, 
     code_challenge_method: "S256",
-    redirect_uri: redirectUri
+    redirect_uri: redirectUri,
+    nonce,
+    scope: 'openid'
   };
 
-  for (const param in queryParams) {
-    linkValue.searchParams.append(param, queryParams[param]);
-  }
+  const getQueryParams = Object.keys(queryParams).map(function (key) {
+			return encodeURIComponent(key) + '=' + encodeURIComponent(queryParams[key]);
+		}).join('&');
 
-  return linkValue
+  const url = endpoint + '?' + getQueryParams
+  return url
 }
 
-// here we build the authentication string that will send us off to IBM App ID
-const state = randomstring.generate()
-const code_verifier = randomstring.generate(128)
-
-// this should be put in at build time and not be kept in repository
-// TODO fix this 
-const clientId = IBM_APP_ID_CLIENTID
-const responseType = 'code'
-const redirectUri='http://localhost:8080/auth'
-const discoveryEndpoint = IBM_APP_ID_DISCOVERY_ENDPOINT
-const baseUrl = "https://eu-gb.appid.cloud.ibm.com/oauth/v4/3143b98e-2035-4fac-b765-879402a1b585/authorization";
-let authentication_url = null;
-
-function getToken() {
-  const tokenEndpoint = new URL(
-    "https://eu-gb.appid.cloud.ibm.com/oauth/v4/3143b98e-2035-4fac-b765-879402a1b585/token"
-  );
-
+async function getOAuthAccessToken() {
   const currentLocation = new URL(window.location.href);
   const authorizationCode = currentLocation.searchParams.get("code");
   const stateFromLocation = currentLocation.searchParams.get("state");
-  const initialCodeVerifier = window.sessionStorage.getItem("code_verifier");
+  const initialCodeVerifier = window.sessionStorage.getItem("codeVerifier");
+  
 
   // This is a good place for checking the state too  
   console.log(window.sessionStorage.getItem("state"))
@@ -88,27 +84,31 @@ function getToken() {
     throw Error("Probable session hijacking attack!");
   }
 
+  console.log("Code: "+authorizationCode)
   const queryParams = {
-    client_id: clientId,
-    grant_type: "authorization_code",
-    state: stateFromLocation,
+    grant_type: 'authorization_code',
+    redirect_uri: redirectUri,
     code: authorizationCode,
-    code_verifier: initialCodeVerifier,
-    redirect_uri: redirectUri
+    code_verifier: initialCodeVerifier    
   };
+  console.log(queryParams)
+  const postBodyContent = Object.keys(queryParams).map(function (key) {
+			return encodeURIComponent(key) + '=' + encodeURIComponent(queryParams[key]);
+		}).join('&');
+  console.log(postBodyContent)
+  console.log(clientId)
+  const secret = jsrsasign.stob64(`${clientId}:${initialCodeVerifier}`)
+  console.log(secret)
 
-  for (const param in queryParams) {
-    tokenEndpoint.searchParams.append(param, queryParams[param]);
-  }
-
-  fetch(`${tokenEndpoint.origin}${tokenEndpoint.pathname}`, {
+  await fetch("https://eu-gb.appid.cloud.ibm.com/oauth/v4/3143b98e-2035-4fac-b765-879402a1b585/token", {
     method: "POST",
-    body: tokenEndpoint.searchParams,
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Accept: "application/json"
-    }
+      'Authorization': 'Basic '+secret,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: postBodyContent
   }).then(response => {
+      console.log(response)
       if (!response.ok) throw Error(response.statusText);
       return response.json();
     })
@@ -116,19 +116,27 @@ function getToken() {
       // save the tokens
       store.state.tokens = json;
       console.log(store)
-      fetchData();
-    });
+    }).catch(err => console.log(err))
 }
 
-function fetchData() {
+function getRandomString(length)
+{
+  return jsrsasign.KJUR.crypto.Util.getRandomHexOfNbytes(length / 2);
+}
+
+async function fetchDataFromPrivateAPI() {
   console.log(store.state.tokens);
-  fetch("https://31f5ff35.eu-gb.apigw.appdomain.cloud/private-authtest/Hello", {
+  const data = await fetch("https://31f5ff35.eu-gb.apigw.appdomain.cloud/private-authtest/Hello", {
     headers: {
-      Authorization: `Bearer ${store.state.tokens.access_token}`
+      Authorization: `Bearer ${store.state.tokens.access_token}`,
+      'Content-type': "application/json"
     }
   }).then((res) => {
-    console.log(res);
+    const data = res.json()
+    console.log(data)
+    return data
   });
+  return data
 }
 
 export default {
@@ -140,27 +148,36 @@ export default {
       authenticationUrl: null
     }
   },
+
+  // notice the async can be applied to the event binding methods for Vue
+  async created() {
+    // When we return from our IBM App ID login page
+    // we then can check the code and state
+    const currentLocation = new URL(window.location.href);
+    const hrefBase = currentLocation.href.substr(0,currentLocation.href.indexOf('?'));
+    if ( hrefBase == redirectUri ) {
+      console.log("REDIRECTED FROM APP ID")
+      await getOAuthAccessToken();
+      const data = await fetchDataFromPrivateAPI();
+      this.private_message = data.message
+    }
+  },
+
   methods: {
     loginWithOAuth2() {
-      // check if we are coming back from our App ID login sequence
-      const currentLocation = new URL(window.location.href);
-      const href_base = currentLocation.href.substr(0,currentLocation.href.indexOf('?'));
-      const stateFromLocation = currentLocation.searchParams.get("state");
-      if (window.sessionStorage.getItem("state") !== stateFromLocation){
-          throw Error("Probable session hijacking attack!");
-      }
-      if ( href_base == redirect_uri ) {
-        console.log("REDIRECTED FROM APP ID")
-        getToken();
-      }
-      
-
-      save_state_and_verifier(state, code_verifier)
-      generateCodeChallenge(code_verifier).then((challenge) => {
+      // this function fires when user clicks a button 
+      const state = getRandomString(20)
+      const codeVerifier = getRandomString(44)
+      console.log("State: "+state)
+      console.log("CodeVerifier: "+codeVerifier)
+      save_state_and_verifier(state, codeVerifier)
+      generatePKCECodeChallenge(codeVerifier).then((challenge) => {
         console.log("Challenge: "+challenge);
         // whatever you generated, example:
         // M5oPBP6RmJPh5QsGtx6ptVH7S2yjqt8sum96jBCyhZg
-        this.authenticationUrl = buildLoginUrl(challenge);
+        this.authenticationUrl = buildLoginUrl(state, challenge);
+
+        window.location.href = this.authenticationUrl;
       });
     }
   }
